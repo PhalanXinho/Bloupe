3b6511783d2270b73600e5f6a5db95bc99896785
haitoo <jiahao20021128@gmail.com>
2023-12-08 18:51:54 +0000

    cap of 2k books

--- Crawler/src/main/java/DownloadTask.java
+++ Crawler/src/main/java/DownloadTask.java
@@ -4,15 +4,17 @@ import org.slf4j.LoggerFactory;
 import java.util.TimerTask;
 
 public class DownloadTask extends TimerTask {
-
+    private int count = 0;
     Logger logger =  LoggerFactory.getLogger(DownloadTask.class);
     private final Controller controller = new Controller();
 
     @Override
     public void run() {
+        if (count == 2000) System.exit(0);
         logger.info("Starting the download task...");
         for (int i = 0; i < 5; i++) {
             if (!controller.run()) i--;
+            count++;
         }
         logger.info("Download task finished");
     }
